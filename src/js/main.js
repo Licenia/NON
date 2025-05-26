f64c1d4be3de0b967032d64d0a22f0462c57ba32
Licenia Alvear <licialvear@gmail.com>
2025-05-26 16:18:40 -0500

    feat(vista-previa): mostrar vista previa del Markdown al hacer click en el botón

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,12 @@ import MarkdownIt from "markdown-it";
 const textMarkdown = document.getElementById("note-textarea");
 const md = new MarkdownIt();
 
+const mostrar = (renderizado) => {
+  const mostrarResultados = document.getElementById("vistaPre");
+  mostrarResultados.innerHTML = "";
+  mostrarResultados.innerHTML = renderizado;
+};
+
 textMarkdown.addEventListener("keyup", () => {
   const texto = textMarkdown.value;
 
@@ -13,9 +19,9 @@ textMarkdown.addEventListener("keyup", () => {
   mostrar(resultadoHtml);
   });
 
-const mostrar = (renderizado) => {
-  const mostrarResultados = document.getElementById("vistaPre");
-  mostrarResultados.innerHTML = "";
-  mostrarResultados.innerHTML = renderizado;
-};
 
+const mostrarVistaPrevia = document.getElementById("vista-previa");
+mostrarVistaPrevia.addEventListener("click", () =>{
+const cambiarEstado = document.getElementById("preview-view");
+cambiarEstado.style.display = "block";
+})
