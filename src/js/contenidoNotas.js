96452ef7212a0177e963f4cc7c7321db62c7a6cd
Licenia Alvear <licialvear@gmail.com>
2025-05-29 11:11:28 -0500

    feat: agregar funcionalidad de ver nota guardada

---
+++ src/js/contenidoNotas.js
@@ -0,0 +1,12 @@
+
+const datosGuardados = JSON.parse(localStorage.getItem("notas"));
+const posicion = localStorage.getItem("cardSeleccionada");
+
+const card = datosGuardados[posicion];
+
+const container = document.getElementById("container");
+const div = document.createElement("div");
+div.innerHTML = `<h3>${card.titulo}</h3>
+<p>${card.fecha}</p>
+<p>${card.contenido}</p>`;
+container.appendChild(div);
--- src/js/note.js
+++ src/js/note.js
@@ -1,29 +1,42 @@
 const contenedorLista = document.querySelector(".contenedor-lista");
 
 const datosGuardados = JSON.parse(localStorage.getItem("notas"));
-datosGuardados.forEach((nota) => {
+datosGuardados.forEach((nota, index) => {
   const containerOpciones = document.createElement("div");
-
+  
   const card = document.createElement("div");
   card.classList.add("card");
-
+  card.setAttribute("id", "card");
+  card.dataset.index = index;
+  
   const titulo = document.createElement("h3");
   titulo.textContent = nota.titulo;
-
+  
   const fecha = document.createElement("small");
   fecha.textContent = nota.fecha;
   fecha.classList.add("date");
-
+  
   const contenido = document.createElement("p");
   contenido.textContent = nota.contenido;
-
+  
   const menu = document.createElement("img");
   menu.setAttribute("src", "/public/assets/images/tres-puntos.png");
   menu.classList.add("menu-btn");
-
+  
   card.appendChild(titulo);
+  card.appendChild(contenido);
   containerOpciones.appendChild(fecha);
   containerOpciones.appendChild(menu);
   contenedorLista.appendChild(card);
   card.appendChild(containerOpciones);
+});
+
+
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    const index = e.currentTarget.dataset.index;
+    localStorage.setItem("cardSeleccionada", index);
+    window.location.href = "contenidoNotas.html";
+  });
 });
